8d269a70e0f41baf8a12ed87f21846fb784bd71d
fizzydrinks <gabriel@lins.dev>
2024-07-30 14:36:43 -0300

    Revert "Add cv-item component"
    This reverts commit fb4858d4cf57c9f319b6bcd681d41ad4ca69b801.

--- src/Bordered/Bordered.ts
+++ src/Bordered/Bordered.ts
@@ -2,7 +2,6 @@ import StyledWebComponent from "@/Styled/StyledWebComponent";
 import { css } from "@emotion/css";
 
 const Bordered = StyledWebComponent.of(css`
-  display: block;
   border: 2px solid var(--border-color);
   border-image: var(--border-gradient) 1;
 `);
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import "./styles/cv.scss";
 
 import BackToTopButton from "./BackToTop/BackToTop";
-import CvItem from "./CvItem/CvItem";
 
 customElements.define("back-to-top", BackToTopButton);
-customElements.define("cv-item", CvItem);
