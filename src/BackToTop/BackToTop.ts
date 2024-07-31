4c284c402b4011853be653414845600091b015c1
fizzydrinks <gabriel@lins.dev>
2024-07-30 21:08:49 -0300

    Remove emotion

--- src/BackToTop/BackToTop.ts
+++ src/BackToTop/BackToTop.ts
@@ -1,33 +1,7 @@
-import Bordered from "@/Bordered/Bordered";
-import { css } from "@emotion/css";
+import './BackToTop.css'
 
-class BackToTopButton extends Bordered.of(css`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  grid-area: b;
-  background: none;
-  font: inherit;
-  color: inherit;
-  box-shadow: none;
-  height: 12em;
-  margin: 6em 0;
-  cursor: pointer;
-  transition: all 300ms ease;
-
-  &:hover {
-    background: var(--hl);
-    color: black;
-  }
-
-  @media print {
-    display: none;
-  }
-`) {
+class BackToTopButton extends HTMLElement {
   connectedCallback() {
-    super.connectedCallback();
-
     this.addEventListener("click", () => {
       window.scroll({
         top: 0,
